16b5c7a93c228ab7b22ece71bb611276dbe00ecc
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-06 19:21:22 +0100

    Add Ball class and ball object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
 import paddle
+import ball
+import time
 
 screen = turtle.Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = paddle.Paddle(paddle_side="right")
 l_paddle = paddle.Paddle(paddle_side="left")
+ball = ball.Ball()
 
 screen.listen()
 screen.onkeypress(r_paddle.move_up, "Up")
@@ -19,6 +22,8 @@ screen.onkeypress(l_paddle.move_down, "s")
 
 is_game_on = True
 while is_game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
